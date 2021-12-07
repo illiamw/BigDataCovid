73dcdf8caa0c2b05988799b12dd7d894b5b49dc4
rodrigoanes18 <39965452+rodrigoanes18@users.noreply.github.com>
2021-12-07 01:18:54 -0300

    Update 2_2EstatisticasEspecificas.sql

--- scripts/2_2EstatisticasEspecificas.sql
+++ scripts/2_2EstatisticasEspecificas.sql
@@ -1,9 +1,9 @@
 -- EXECICIO 2 - 2
---  Qual a quantidade de pacientes presente na base de dados?
--- Quantos s˜ao homens e quantos s˜ao mulheres?
+-- Qual a quantidade de pacientes presente na base de dados?
+-- Quantos são homens e quantos são mulheres?
 SELECT COUNT(*) FROM pacientes;
 SELECT "IC_SEXO", COUNT(*) AS NUM FROM pacientes GROUP BY "IC_SEXO";
--- Qual ´e faixa et´aria dos pacientes homens e mulheres?
+-- Qual é faixa etária dos pacientes homens e mulheres?
 SELECT ( (2021-MIN("AA_NASCIMENTO")) - (2021-MAX("AA_NASCIMENTO"))) AS amplitude,
 (2021-MIN("AA_NASCIMENTO")) AS velho,
 (2021-MAX("AA_NASCIMENTO")) AS novo
@@ -12,7 +12,7 @@ SELECT ( (2021-MIN("AA_NASCIMENTO")) - (2021-MAX("AA_NASCIMENTO"))) AS amplitude
 (2021-MIN("AA_NASCIMENTO")) AS velho,
 (2021-MAX("AA_NASCIMENTO")) AS novo
 FROM pacientes WHERE "IC_SEXO" = 'F';
---  Qual a distribui¸c˜ao dos quartis dentro de cada faixa?
+--  Qual a distribuição dos quartis dentro de cada faixa?
 SELECT
 (2021 - MAX("AA_NASCIMENTO")) AS minimo,
 (2021 - PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY "AA_NASCIMENTO")) AS Q1,
@@ -20,7 +20,7 @@ SELECT
 (2021 - PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY "AA_NASCIMENTO")) AS Q3,
 (2021 - MIN("AA_NASCIMENTO")) AS maximo
 FROM pacientes;
---  Qual a distribui¸c˜ao em cada gˆenero por d´ecada de vida?
+--  Qual a distribuição em cada gênero por década de vida?
 
 SELECT 
 COUNT(*) FILTER (WHERE "AA_NASCIMENTO" <= 2020 AND "AA_NASCIMENTO" > (2010))   AS "1-10",
@@ -34,7 +34,7 @@ COUNT(*) FILTER (WHERE "AA_NASCIMENTO" <= 1950 AND "AA_NASCIMENTO" > (1940))   A
 COUNT(*) FILTER (WHERE "AA_NASCIMENTO" <= 1940 AND "AA_NASCIMENTO" > (1930))   AS "80-90"
 FROM pacientes;
 
--- Qual a maior quantidade de exames solicitados para um ´unico
+-- Qual a maior quantidade de exames solicitados para um único
 -- paciente ?
 
 
@@ -47,7 +47,7 @@ SELECT "ID_PACIENTE",
 	COUNT(*) AS NUM FROM exames GROUP BY "ID_PACIENTE"
 Order by NUM DESC;
 
---  Qual ´e a m´edia de exames pedidos para homens e para mulheres?
+-- Qual é a média de exames pedidos para homens e para mulheres?
 
 SELECT "IC_SEXO", AVG(subquery.countexames)
 FROM(
@@ -58,7 +58,7 @@ FROM exames
 INNER JOIN pacientes ON subquery."ID_PACIENTE" = pacientes."ID_PACIENTE"
 GROUP BY "IC_SEXO"
 
---  Quantos exames de Coronav´ırus (2019-nCoV) foram solicitados?
+-- Quantos exames de Coronavírus (2019-nCoV) foram solicitados?
 -- Quantos deles apresentam resultado positivo?
 
 SELECT *
@@ -105,7 +105,20 @@ WHERE "DE_ANALITO" = 'Covid 19, Anticorpos IgA'
 ORDER BY "DE_RESULTADO";
 
 
+-- Qual é o desfecho para a maioria dos casos registrados?
 
+SELECT "DE_DESFECHO", COUNT(*) AS NUM FROM desfechos GROUP BY "DE_DESFECHO" ORDER BY NUM DESC;
 
+-- E para cada distribuição por gênero e por década de vida?
 
-
+SELECT 
+* FILTER (WHERE "AA_NASCIMENTO" <= 2020 AND "AA_NASCIMENTO" > (2010))   AS "1-10",
+* FILTER (WHERE "AA_NASCIMENTO" <= 2010 AND "AA_NASCIMENTO" > (2000))   AS "10-20",
+* FILTER (WHERE "AA_NASCIMENTO" <= 2000 AND "AA_NASCIMENTO" > (1990))   AS "20-30",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1990 AND "AA_NASCIMENTO" > (1980))   AS "30-40",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1980 AND "AA_NASCIMENTO" > (1970))   AS "40-50",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1970 AND "AA_NASCIMENTO" > (1960))   AS "50-60",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1960 AND "AA_NASCIMENTO" > (1950))   AS "60-70",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1950 AND "AA_NASCIMENTO" > (1940))   AS "70-80",
+* FILTER (WHERE "AA_NASCIMENTO" <= 1940 AND "AA_NASCIMENTO" > (1930))   AS "80-90"
+FROM pacientes;
