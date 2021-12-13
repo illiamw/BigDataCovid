e8b3b666583f951bae6f73fbac9521b1a4240c47
William Ferreira <william.luis.ferreira@usp.br>
2021-12-13 15:55:39 -0300

    Fechando 5 e 8

--- scripts/5_Hemograma.sql
+++ scripts/5_Hemograma.sql
@@ -1,3 +1,11 @@
-select distinct "DE_ANALITO" from exames
+-- Escreva uma consulta que mostre quais analitos podem ser medidos em exames de ’hemograma’, em cada hospital.
+select distinct "DE_ORIGEM", "DE_ANALITO"
+from exames
 WHERE ("DE_EXAME"= 'Hemograma')
-GROUP BY "DE_ANALITO";
+order by "DE_ORIGEM",  "DE_ANALITO";
+
+-- Compare os nomes dos analitos entre os diferentes hospitais, e execute um processo de atualização dos nomes, corrigindo e integrando as variantes e grafias ´obvias.
+select distinct "DE_ANALITO"
+from exames
+WHERE ("DE_EXAME"= 'Hemograma')
+order by  "DE_ANALITO";
